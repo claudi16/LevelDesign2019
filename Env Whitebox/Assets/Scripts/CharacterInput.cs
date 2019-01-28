f31bda43d6e3a2bb1c2a82286d15f1e9bbc8aa56
claudi <claudi1618@gmail.com>
2019-01-28 09:17:35 -0700

    Pass8

---
+++ Env Whitebox/Assets/Scripts/CharacterInput.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class CharacterInput : MonoBehaviour {
+	public float speed = 30;
+	public bool PlayerInput = true;
+	void Start () {
+		StartCoroutine(CharInput());
+	}
+
+	IEnumerator CharInput(){
+		while(PlayerInput){
+			if(Input.GetKey(KeyCode.RightArrow)){
+			transform.Translate(Vector3.right * Time.deltaTime * speed);
+			}
+			if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.Translate(Vector3.left * Time.deltaTime * speed);
+			}
+			if(Input.GetKey(KeyCode.UpArrow)){
+			transform.Translate(Vector3.forward * Time.deltaTime * speed);
+			}
+			if(Input.GetKey(KeyCode.DownArrow)){
+			transform.Translate(Vector3.back * Time.deltaTime * speed);
+			}
+		yield return new WaitForSeconds(0.01f);
+
+		}
+	}
+}
+
+	/* void Update(){
+		if(Input.GetKeyDown(KeyCode.RightArrow)){
+		transform.Translate(Vector3.right * Time.deltaTime * speed);
+		}
+	}
+}*/
